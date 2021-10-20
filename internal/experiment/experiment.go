89f973bd6a601057be586aad9301be5b316a9612
Vallghall <romangusev2001@mail.ru>
2021-10-20 22:28:31 +0000

    [ADD-5] Fixes

--- internal/experiment/experiment.go
+++ internal/experiment/experiment.go
@@ -1,18 +1,44 @@
 package experiment
 
+import (
+	"os"
+
+	"gitlab.com/Valghall/diwor/internal/crypto"
+)
+
 type experiment struct {
-	InitialData *initialData
+	initialData *initialData
+	encrypted   []byte
+}
+
+func (e *experiment) InitialData() *initialData {
+	return e.initialData
+}
+
+func (e *experiment) SetInitialData(initialData *initialData) {
+	e.initialData = initialData
+}
+
+func (e *experiment) Encrypted() []byte {
+	return e.encrypted
+}
+
+func (e *experiment) SetEncrypted(encrypted []byte) {
+	e.encrypted = encrypted
 }
 
 func (i *initialData) NewExperiment() *experiment {
 	return &experiment{
-		InitialData: i,
+		initialData: i,
 	}
 }
 
-/*
 func (e *experiment) Start() {
 	plaintext, _ := os.ReadFile("./internal/experiment/original_text_sample.txt")
-	crypto.Encrypt(plaintext, e.InitialData.Mode, e.InitialData.SampleA.Algorithm(), e.InitialData.SampleA.BlockSize())
+	encrypted, _ := crypto.Encrypt(plaintext,
+		e.InitialData().Mode,
+		e.InitialData().SampleA.Cipher(),
+		e.InitialData().SampleA.BlockSize(),
+	)
+	e.SetEncrypted(encrypted)
 }
-*/
